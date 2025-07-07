c7949b45eb312c26dc97e69844386a1ff334ed15
Kainat Kiran Rashid <97904362+kainatkiranrashid2@users.noreply.github.com>
2025-07-07 18:39:10 +0500

    Apple-style Logos Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import LogosSection from "./components/LogosSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <LogosSection />
     </main>
   );
 }
